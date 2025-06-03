a52e2f0628f2e15f747263da7dbde7b6b3a7275f
Pankaj chejara <pankajchejara23@gmail.com>
2025-06-03 14:03:25 +0300

    adding static url

--- edu_traject/urls.py
+++ edu_traject/urls.py
@@ -19,10 +19,16 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import change_language
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls")),
     path("trajectory/",include("teacher.urls")),
+    path("student/",include("student.urls")),
 ]
 
 urlpatterns += [ path('change-lang/<lang_code>', change_language, name='change_language')]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
